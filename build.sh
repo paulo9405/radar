19fef16b4525b1b5a96706b7e95cd815a2a87f0a
Paulo Gomes <paulo.ricardo1137.pr@mail.com>
2026-05-24 11:10:33 -0300

    Add automatic superuser creation on deploy

--- build.sh
+++ build.sh
@@ -10,3 +10,8 @@ python manage.py collectstatic --noinput
 
 # Run database migrations
 python manage.py migrate
+
+# Create superuser automatically if environment variables are set
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ]; then
+    python manage.py createsuperuser --noinput || true
+fi
